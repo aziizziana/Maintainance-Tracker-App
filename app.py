e03fefca4fbeb1ab879220dd13804e5780686999
Ziana <aziizziana@gmail.com>
2018-07-10 18:15:59 +0300

    created routes

---
+++ app.py
@@ -0,0 +1,24 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+
+@app.route('/')
+def main():
+    return render_template('index.html')
+
+@app.route('/signup')
+def signUp():
+    return render_template('signup.html')
+
+@app.route('/reportproblem')
+def reportProblem():
+    return render_template('report_problem.html')
+
+@app.route('/approve')
+def approve():
+    return render_template('approve.html')
+
+if __name__ == '__main__':
+    app.run()
+
+
+
